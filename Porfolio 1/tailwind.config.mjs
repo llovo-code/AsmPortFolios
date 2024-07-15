afdbbeb9d26a213b0390c7a3ca3d81b847d819ba
llovo-code <lewinjoselovo@gmail.com>
2024-07-15 02:41:09 -0600

    feat: added responsive and format code

--- Porfolio 1/tailwind.config.mjs
+++ Porfolio 1/tailwind.config.mjs
@@ -3,7 +3,21 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			keyframes:{
+				'fade-in': {
+					"0%": {
+						opacity: 0
+					},
+					"100%": {
+						opacity: 1
+					}
+				}
+			},
+			animation:{
+				'fade-in': 'fade-in 2s ease-in-out'
+			}
+		},
 	},
 	plugins: [],
 }
